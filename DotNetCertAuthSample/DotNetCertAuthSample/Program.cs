2e3fbe442970a9101fca11ed5578c60422359945
igal <igalfsg@gmail.com>
2021-11-10 16:38:59 -0500

    Added C# Example

---
+++ DotNetCertAuthSample/DotNetCertAuthSample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetCertAuthSample.Services;
+using DotNetCertAuthSample.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DotNetCertAuthSample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            HttpClient _httpClient = new ();
+            HTTPService _httpService = new(_httpClient);
+            //prefered option subject name based
+            X509Certificate2? cert = GetCertFromWinStoreBySubject("keytos.io");
+            //thumbprint option
+            cert = GetCertFromWinStoreBythumbprint("e00cee63cadf08c80de33e7bc44c89027c164aeb");
+            if(cert == null)
+            {
+                Console.WriteLine("Could not find certificate");
+                return;
+            }
+            APIResultModel result = await _httpService.SendGetAsync(
+                "https://localhost:5001/api/Certificates/RenewCertificate"
+                , cert);
+            return;
+        }
+
+        private static X509Certificate2 GetCertFromWinStoreBySubject(string subjectName)
+        {
+            X509Store store = new (StoreLocation.CurrentUser);
+            X509Certificate2? cert = null;
+            store.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certs = store.Certificates.Find(
+                X509FindType.FindBySubjectName, subjectName, true);
+            if(certs.Count > 0)
+            {
+                cert = certs[0];
+            }
+            return cert;
+        }
+
+        private static X509Certificate2 GetCertFromWinStoreBythumbprint(string thumbprint)
+        {
+            //not recommended since it breaks with auto rotation
+            X509Store store = new (StoreLocation.CurrentUser);
+            X509Certificate2? cert = null;
+            store.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certs = store.Certificates.Find(
+                X509FindType.FindByThumbprint, thumbprint, true);
+            if (certs.Count > 0)
+            {
+                cert = certs[0];
+            }
+            return cert;
+        }
+    }
+}
---
+++ DotNetCertAuthSample/DotNetCertAuthSample/Services/HTTPService.cs
@@ -0,0 +1,110 @@
+﻿using DotNetCertAuthSample.Models;
+using Jose;
+using Polly;
+using Polly.Retry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCertAuthSample.Services
+{
+    public class HTTPService
+    {
+        private TokenModel? _token;
+        private readonly HttpClient _httpClient;
+        private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+        public HTTPService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            HttpStatusCode[] httpStatusCodesWorthRetrying = {
+               HttpStatusCode.RequestTimeout, // 408
+               HttpStatusCode.InternalServerError, // 500
+               HttpStatusCode.BadGateway, // 502
+               HttpStatusCode.ServiceUnavailable, // 503
+               HttpStatusCode.GatewayTimeout // 504
+            };
+            _retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .OrInner<TaskCanceledException>()
+                .OrResult<HttpResponseMessage>(r => httpStatusCodesWorthRetrying.Contains(r.StatusCode))
+                  .WaitAndRetryAsync(new[]
+                  {
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(4),
+                    TimeSpan.FromSeconds(8)
+                  });
+        }
+
+        private void GetTokenAsync(X509Certificate2 clientCertificate)
+        {
+            if(_token == null || _token.ExpiresOn < DateTimeOffset.Now)
+            {
+                CreateRSAJWTToken(clientCertificate);
+            }
+        }
+
+        public async Task<APIResultModel> SendGetAsync(string url, X509Certificate2 clientCertificate)
+        {
+            GetTokenAsync(clientCertificate);
+            APIResultModel apiResult = new ();
+            HttpResponseMessage responseMessage;
+            HttpRequestMessage requestMessage = new (HttpMethod.Get, url);
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", _token.AccessToken);
+            try
+            {
+                responseMessage = await _retryPolicy.ExecuteAsync(async () =>
+                          await SendMessageAsync(requestMessage));
+                apiResult.Message = await responseMessage.Content.ReadAsStringAsync();
+                apiResult.Success = responseMessage.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                apiResult.Success = false;
+                if (ex.Message.Contains("One or more errors") 
+                    && ex.InnerException != null)
+                {
+                    apiResult.Message = ex.InnerException.Message;
+                }
+                else
+                {
+                    apiResult.Message = ex.Message;
+                }
+            }
+            return apiResult;
+        }
+
+
+        private void CreateRSAJWTToken(X509Certificate2 clientCertificate)
+        {
+            var headers = new Dictionary<string, object>
+            {
+                { "typ", "JWT" },
+                { "x5t", clientCertificate.Thumbprint }
+            };
+            _token = new();
+            var payload = new Dictionary<string, object>()
+            {
+                {"aud", $"https://ezca.io"},
+                {"jti", Guid.NewGuid().ToString()},
+                {"nbf", (ulong)_token.NotBefore.ToUnixTimeSeconds()},
+                {"exp", (ulong)_token.ExpiresOn.ToUnixTimeSeconds()}
+            };
+            _token.AccessToken =  JWT.Encode(payload, clientCertificate.GetRSAPrivateKey(), 
+                JwsAlgorithm.RS256, extraHeaders: headers);
+        }
+
+        private async Task<HttpResponseMessage> SendMessageAsync(HttpRequestMessage requestMessage)
+        {
+            HttpResponseMessage response;
+            response = await _httpClient.SendAsync(requestMessage);
+            return response;
+        }
+
+    }
+}
