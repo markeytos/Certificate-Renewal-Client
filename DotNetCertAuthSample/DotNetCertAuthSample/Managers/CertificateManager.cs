88413782033c69f02d6e447a21ea562b70147847
igal <igalfsg@gmail.com>
2023-01-28 16:51:10 -0500

    Updated To External NuGet Package, added RDP certificate setting

---
+++ DotNetCertAuthSample/DotNetCertAuthSample/Managers/CertificateManager.cs
@@ -0,0 +1,359 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using CERTENROLLLib;
+using CommandLine;
+using DotNetCertAuthSample.Models;
+using DotNetCertAuthSample.Services;
+using EZCAClient.Managers;
+using EZCAClient.Models;
+using EZCAClient.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Management.Infrastructure;
+using Microsoft.Management.Infrastructure.Options;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DotNetCertAuthSample.Managers;
+
+public class CertificateManager
+{
+    private ILogger? _logger;
+    private RenewArgModel? _renewArgModel;
+    private GenerateArgModel? _generateArgModel;
+    private RegisterArgModel? _registerArgModel;
+
+
+    public int InitializeManager(RenewArgModel values)
+    {
+        _logger = CreateLogger(values.AppInsightsKey);
+        _renewArgModel = values;
+        return 0;
+    }
+
+    public int InitializeManager(GenerateArgModel values)
+    {
+        _logger = CreateLogger(values.AppInsightsKey);
+        _generateArgModel = values;
+        return 0;
+    }
+
+    public int InitializeManager(RegisterArgModel values)
+    {
+        _logger = CreateLogger(values.AppInsightsKey);
+        _registerArgModel = values;
+        return 0;
+    }
+
+    public int ProcessError(IEnumerable<Error> errs)
+    {
+        return 1;
+    }
+
+    public async Task<int> CallCertActionAsync()
+    {
+        if( _renewArgModel != null )
+        {
+            return await RenewAsync(_renewArgModel);
+        }
+        if( _generateArgModel != null )
+        {
+            return await CreateCertAsync(_generateArgModel);
+        }
+        if (_registerArgModel != null)
+        {
+            return await RegisterAndCreateCertAsync(_registerArgModel);
+        }
+        return -1;
+    }
+
+    private async Task<int> RenewAsync(RenewArgModel values)
+    {
+        if(_logger == null)
+        {
+            throw new ArgumentNullException(nameof(_logger));
+        }
+        try
+        {
+            if (values.RDPCert && values.LocalCertStore == false)
+            {
+                throw new ArgumentException(
+                    "If certificate will be used for RDP it must be stored in the local store");
+            }
+            if (string.IsNullOrWhiteSpace(values.Domain))
+            {
+                throw new ArgumentNullException(nameof(values.Domain));
+            }
+            X509Certificate2 cert = WindowsCertStoreService.GetCertFromWinStoreBySubject(
+                values.Domain.Replace("CN=", ""), values.LocalCertStore);
+            CX509CertificateRequestPkcs10 certRequest = WindowsCertStoreService.CreateCSR(
+                "CN=" + values.Domain, GetSujectAlternativeName(cert), 4096, values.LocalCertStore);
+            string csr = certRequest.RawData[EncodingType.XCN_CRYPT_STRING_BASE64REQUESTHEADER];
+            _logger.LogInformation($"Renewing certificate");
+            IEZCAClient ezcaClient = new EZCAClientClass(
+                new HttpClient(), _logger, values.url);
+            string createdCert = await ezcaClient.RenewCertificateAsync(cert, csr);
+            WindowsCertStoreService.InstallCertificate(createdCert, certRequest);
+            _logger.LogInformation($"certificate {values.Domain} was renewed successfully");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error renewing certificate {values.Domain}");
+            return 1;
+        }
+        return 0;
+    }
+
+    private async Task<int> CreateCertAsync(GenerateArgModel values)
+    {
+        if (_logger == null)
+        {
+            throw new ArgumentNullException(nameof(_logger));
+        }
+        try
+        {
+            if (values.RDPCert && values.LocalCertStore == false)
+            {
+                throw new ArgumentException(
+                    "If certificate will be used for RDP it must be stored in the local store");
+            }
+            if (!IsGuid(values.caID))
+            {
+                throw new ArgumentException("Please enter a valid CA ID Guid");
+            }
+            if (string.IsNullOrWhiteSpace(values.Domain))
+            {
+                throw new ArgumentNullException(nameof(values.Domain));
+            }
+            IEZCAClient ezcaClient = new EZCAClientClass(
+                new HttpClient(), _logger, values.url, CreateTokenCredential(
+                    values.ClientID, values.ClientSecret, values.TenantID));
+            _logger.LogInformation("Getting available CAs");
+            AvailableCAModel selectedCA = await GetCAAsync(values.caID, ezcaClient);
+            X509Certificate2 createdCertificate = await CreateCertificateAsync(
+                values.Domain, values.LocalCertStore, selectedCA,
+                values.Validity, ezcaClient);
+            if (values.RDPCert)
+            {
+                SetRDPCertificate(createdCertificate.Thumbprint);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error creating certificate");
+            return 1;
+        }
+        return 0;
+    }
+
+    private async Task<int> RegisterAndCreateCertAsync(RegisterArgModel values)
+    {
+        if (_logger == null)
+        {
+            throw new ArgumentNullException(nameof(_logger));
+        }
+        try
+        {
+            if (values.RDPCert && values.LocalCertStore == false)
+            {
+                throw new ArgumentException(
+                    "If certificate will be used for RDP it must be stored in the local store");
+            }
+            if (!IsGuid(values.caID))
+            {
+                throw new ArgumentException("Please enter a valid CA ID Guid");
+            }
+            if (string.IsNullOrWhiteSpace(values.Domain))
+            {
+                throw new ArgumentNullException(nameof(values.Domain));
+            }
+            IEZCAClient ezcaClient = new EZCAClientClass(new HttpClient(), 
+                _logger, values.url, CreateTokenCredential(
+                    values.ClientID, values.ClientSecret, values.TenantID));
+            _logger.LogInformation("Getting available CAs");
+            AvailableCAModel selectedCA = await GetCAAsync(values.caID, ezcaClient);
+            APIResultModel registrationResult = await ezcaClient.RegisterDomainAsync(
+                selectedCA, values.Domain);
+            _logger.LogInformation($"Registering domain: {values.Domain}");
+            if (!registrationResult.Success)
+            {
+                throw new InvalidOperationException(
+                    $"Could not register new domain in EZCA {registrationResult.Message}");
+            }
+            _logger.LogInformation($"Successfully registered domain: {values.Domain}");
+            X509Certificate2 createdCertificate = await CreateCertificateAsync(
+                values.Domain, values.LocalCertStore, selectedCA,
+                values.Validity, ezcaClient);
+            if(values.RDPCert)
+            {
+                SetRDPCertificate(createdCertificate.Thumbprint);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error registering certificate");
+            return 1;
+        }
+        return 0;
+    }
+
+    private void SetRDPCertificate(string thumbprint)
+    {
+        string namespaceValue = @"root\cimv2\TerminalServices";
+        string queryDialect = "WQL";
+        string query = "SELECT * FROM Win32_TSGeneralSetting WHERE TerminalName = 'RDP-Tcp'";
+        string thumbprintProperty = "SSLCertificateSHA1Hash";
+        var dComOpts = new DComSessionOptions()
+        {
+            Culture = CultureInfo.CurrentCulture,
+            UICulture = CultureInfo.CurrentUICulture,
+            PacketIntegrity = true,
+            PacketPrivacy = true,
+            Timeout = new TimeSpan(0)
+        };
+        CimSession cimSession = CimSession.Create("localhost", dComOpts);
+        CimInstance? instance = cimSession.QueryInstances(namespaceValue,
+            queryDialect, query).ToArray().FirstOrDefault();
+        if (instance == null)
+        {
+            throw new Exception("Error getting RDP service");
+        }
+        var check = !instance.CimInstanceProperties[thumbprintProperty].Value.Equals(thumbprint);
+        if (check)
+        {
+            var prop = instance.CimInstanceProperties[thumbprintProperty];
+            prop.Value = thumbprint;
+            cimSession.ModifyInstance(instance);
+        }
+    }
+
+    private  async Task<X509Certificate2> CreateCertificateAsync(string domain, bool localStore,
+        AvailableCAModel selectedCA, int validity, IEZCAClient ezcaClient)
+    {
+        if (_logger == null)
+        {
+            throw new ArgumentNullException(nameof(_logger));
+        }
+        if (validity <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Error certificate validity has to be greater than 0");
+        }
+        List<string> subjectAltNames = new List<string>
+        {
+            domain
+        };
+        CX509CertificateRequestPkcs10 certRequest = WindowsCertStoreService.CreateCSR("CN=" + domain,
+            subjectAltNames, 4096, localStore);
+        string csr = certRequest.RawData[EncodingType.XCN_CRYPT_STRING_BASE64REQUESTHEADER];
+        _logger.LogInformation($"Getting Windows certificate for {domain}");
+        X509Certificate2? windowsCert = await ezcaClient.RequestCertificateAsync(selectedCA, csr, domain, 20);
+        if (windowsCert != null)
+        {
+            _logger.LogInformation($"Installing Windows Certificate for " +
+                $"{domain} with thumbprint {windowsCert.Thumbprint}");
+            WindowsCertStoreService.InstallCertificate(
+                CryptoStaticService.ExportToPEM(windowsCert), certRequest);
+            _logger.LogInformation($"Successfully created certificate for " +
+                $"{domain} with thumbprint {windowsCert.Thumbprint}");
+            return windowsCert;
+        }
+        else
+        {
+            throw new CryptographicException($"Error requesting EZCA certificate for {domain}");
+        }
+    }
+
+    private static async Task<AvailableCAModel> GetCAAsync(string caID, IEZCAClient ezcaClient)
+    {
+        AvailableCAModel[]? availableCAs = await ezcaClient.GetAvailableCAsAsync();
+        if (availableCAs == null || availableCAs.Any() == false)
+        {
+            throw new NullReferenceException("Could not find any available CAs in EZCA");
+        }
+        AvailableCAModel? selectedCA = availableCAs.FirstOrDefault(i => i.CAID == caID);
+        if (selectedCA == null)
+        {
+            throw new ArgumentOutOfRangeException(
+                $"No CA with CA ID {caID} was found, make sure you have access to request from this CA");
+        }
+        return selectedCA;
+    }
+
+    private static TokenCredential? CreateTokenCredential(string clientID, 
+        string clientSecret, string tenantID)
+    {
+        if(string.IsNullOrWhiteSpace(clientID)
+            || string.IsNullOrWhiteSpace(clientSecret)
+            || string.IsNullOrWhiteSpace(tenantID))
+        {
+            return null;
+        }
+        return new ClientSecretCredential(tenantID, clientID, clientSecret);
+    }
+
+    private static ILogger CreateLogger(string? appInsightsKey)
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddLogging(builder =>
+        {
+            if (!string.IsNullOrWhiteSpace(appInsightsKey))
+            {
+                builder.AddApplicationInsights(
+                configureTelemetryConfiguration: (config) => config.ConnectionString =
+                appInsightsKey,
+                configureApplicationInsightsLoggerOptions: (options) => { });
+            }
+            builder.AddEventLog();
+        });
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        return serviceProvider.GetRequiredService<ILogger<Program>>();
+    }
+
+    public static bool IsGuid(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return false;
+        }
+        Guid x;
+        return Guid.TryParse(value, out x);
+    }
+
+    private static List<string> GetSujectAlternativeName(X509Certificate2 cert)
+    {
+        var result = new List<string>();
+        var subjectAlternativeName = cert.Extensions.Cast<X509Extension>()
+                                            .Where(n => n.Oid?.Value == "2.5.29.17")
+                                            .Select(n => new AsnEncodedData(n.Oid, n.RawData))
+                                            .Select(n => n.Format(true))
+                                            .FirstOrDefault();
+        if (subjectAlternativeName != null)
+        {
+            var alternativeNames = subjectAlternativeName.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            foreach (var alternativeName in alternativeNames)
+            {
+                var groups = Regex.Match(alternativeName, @"^(.*)=(.*)").Groups; // @"^DNS Name=(.*)").Groups;
+                var groups2 = Regex.Match(alternativeName, @"^(.*):(.*)").Groups; // @"^DNS Name:(.*)").Groups;
+                if (groups.Count > 1 && !string.IsNullOrWhiteSpace(groups[2].Value))
+                {
+                    result.Add(groups[2].Value.Trim());
+                }
+                else if (groups2.Count > 1 && !string.IsNullOrWhiteSpace(groups2[2].Value))
+                {
+                    result.Add(groups2[2].Value.Trim());
+                }
+            }
+        }
+        return result;
+    }
+
+}
---
+++ DotNetCertAuthSample/DotNetCertAuthSample/Models/RenewArgModel.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine.Text;
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCertAuthSample.Models
+{
+    [Verb("renew", HelpText = "Renews an existing certificate")]
+    public class RenewArgModel
+    {
+        [Option('r',"RDP", Required = false, Default = false,
+            HelpText = "whether this certificate should be added as the computer's RDP certificate")]
+        public bool RDPCert { get; set; }
+
+        [Option('d', "Domain", Required = true,
+            HelpText = "Domain for the certificate you want to create")]
+        public string? Domain { get; set; }
+        [Option("AppInsights", Required = false,
+            HelpText = "Azure Application Insights connection string to send logs to")]
+        public string? AppInsightsKey { get; set; }
+        [Option('e', "EZCAInstance", Required = false, Default = "https://portal.ezca.io/",
+           HelpText = "EZCA instance url")]
+        public string url { get; set; } = "https://portal.ezca.io/";
+        [Option("LocalStore", Required = false, Default = false,
+            HelpText = "If the certificate should be stored in the computers Local Store. If false certificate will be stored in the user store")]
+        public bool LocalCertStore { get; set; }
+        
+    }
+}
