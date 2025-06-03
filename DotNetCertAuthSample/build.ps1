2bb21302615114964d688c3f69dd253376091103
Igal <igalfsg@gmail.com>
2025-06-03 15:40:52 -0400

    added path

--- DotNetCertAuthSample/build.ps1
+++ DotNetCertAuthSample/build.ps1
@@ -3,6 +3,6 @@ param (
     [string] $signingAKV = "https://codesigningkeytos.vault.azure.net/"
 )
 
-msbuild /restore /t:publish  /p:Configuration=Release /p:SelfContained=True  /p:RuntimeIdentifier=win-x64 /p:PublishSingleFile=true
+msbuild .\DotNetCertAuthSample\DotNetCertAuthSample\DotNetCertAuthSample.csproj /restore /t:publish  /p:Configuration=Release /p:SelfContained=True  /p:RuntimeIdentifier=win-x64 /p:PublishSingleFile=true
 $akvToken = (az account get-access-token  --resource https://vault.azure.net --query "accessToken").Replace('"','')
 azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\DotNetCertAuthSample\bin\Release\net8.0-windows\win-x64\publish\*.exe
