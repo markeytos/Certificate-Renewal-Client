227224e55e65c81fc8a50c76d126ed3c23950af6
Igal <igalfsg@gmail.com>
2025-06-03 15:48:54 -0400

    fixed sign?

--- DotNetCertAuthSample/build.ps1
+++ DotNetCertAuthSample/build.ps1
@@ -5,4 +5,4 @@ param (
 
 msbuild .\DotNetCertAuthSample\DotNetCertAuthSample\DotNetCertAuthSample.csproj /restore /t:publish  /p:Configuration=Release /p:SelfContained=True  /p:RuntimeIdentifier=win-x64 /p:PublishSingleFile=true
 $akvToken = (az account get-access-token  --resource https://vault.azure.net --query "accessToken").Replace('"','')
-azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\DotNetCertAuthSample\bin\Release\net8.0-windows\win-x64\publish\*.exe
+azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\DotNetCertAuthSample\DotNetCertAuthSample\bin\Release\net8.0-windows\win-x64\publish\*.exe
